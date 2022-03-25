10c931935a184c42f89dad64892d3368cffdd71d
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2022-03-25 20:38:43 +0600

    slightly done

---
+++ index.php
@@ -0,0 +1,226 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>E-Learning Site</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="WebUni Education Template">
+	<meta name="keywords" content="webuni, education, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/head.jpg" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-3">
+					<div class="site-logo">
+						<img src="img/main-logo.png" alt="">
+					</div>
+					<div class="nav-switch">
+						<i class="fa fa-bars"></i>
+					</div>
+				</div>
+				<div class="col-lg-9 col-md-9">
+					<a href="" class="site-btn header-btn">Login</a>
+					<nav class="main-menu">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="courses.php">Courses</a></li>
+							<li><a href="blog.php">News</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="#">About us</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Hero section -->
+	<section class="hero-section set-bg" data-setbg="img/bg.jpg">
+		<div class="container">
+			<div class="hero-text text-white">
+				<h2>Get The Best Online Courses Here</h2>
+				<p>Students do not learn much just sitting in classes listening to teachers, memorizing prepackaged assignments, and spitting out answers. They must talk about what they are learning, write reflectively about it, relate it to past experiences, and apply it to their daily lives. They must make what they learn part of themselves.” - Arthur W. Chickering and Stephen C. Ehrmann</p>
+			</div>
+			<div class="row">
+				<div class="col-lg-10 offset-lg-1">
+					<form class="intro-newslatter">
+						<button class="site-btn">Sign Up Now</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+
+	<!-- categories section -->
+	<section class="categories-section spad">
+		<div class="container">
+			<div class="section-title">
+				<h2>Our Course Categories</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
+			</div>
+			<div class="row">
+				<!-- categorie -->
+				<div class="col-lg-4 col-md-6">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="img/categories/1.jpg"></div>
+						<div class="ci-text">
+							<h5>IT Development</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur</p>
+							<span>120 Courses</span>
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4 col-md-6">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="img/categories/2.jpg"></div>
+						<div class="ci-text">
+							<h5>Web Design</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur</p>
+							<span>70 Courses</span>
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4 col-md-6">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="img/categories/3.jpg"></div>
+						<div class="ci-text">
+							<h5>Illustration & Drawing</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur</p>
+							<span>55 Courses</span>
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4 col-md-6">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="img/categories/4.jpg"></div>
+						<div class="ci-text">
+							<h5>Social Media</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur</p>
+							<span>40 Courses</span>
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4 col-md-6">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="img/categories/5.jpg"></div>
+						<div class="ci-text">
+							<h5>Photoshop</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur</p>
+							<span>220 Courses</span>
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4 col-md-6">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="img/categories/6.jpg"></div>
+						<div class="ci-text">
+							<h5>Cryptocurrencies</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur</p>
+							<span>25 Courses</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- categories section end -->
+
+
+
+	<!-- footer section -->
+	<footer class="footer-section spad">
+		<div class="footer-top">
+			<div class="footer-warp">
+				<div class="row">
+					<div class="widget-item">
+						<h4>Contact Info</h4>
+						<ul class="contact-list">
+							<li>Chittagong, Bangladesh</li>
+							<li>+880 181 8786108</li>
+							<li>alpharfan07@gmail.com</li>
+						</ul>
+					</div>
+					<div class="widget-item">
+						<h4>Coding</h4>
+						<ul>
+							<li><a href="">C/C++</a></li>
+							<li><a href="">Python</a></li>
+							<li><a href="">JavaScript</a></li>
+							<li><a href="">C#</a></li>
+							<li><a href="">Java</a></li>
+						</ul>
+					</div>
+					<div class="widget-item">
+						<h4>Graphic Design</h4>
+						<ul>
+							<li><a href="">Adobe Illustrator</a></li>
+							<li><a href="">Adobe Photoshop</a></li>
+							<li><a href="">Adobe UI/UX</a></li>
+							<li><a href="">Adobe InDesign</a></li>
+							<li><a href="">Adobe Premier</a></li>
+						</ul>
+					</div>
+					<div class="widget-item">
+						<h4>Development</h4>
+						<ul>
+							<li><a href="">MERN Stack</a></li>
+							<li><a href="">PHP and Laravel</a></li>
+							<li><a href="">Python and Django</a></li>
+							<li><a href="">Asp.net</a></li>
+							<li><a href="">Flutter</a></li>
+						</ul>
+					</div>
+					<div class="widget-item">
+						<h4>Newsletter</h4>
+						<form class="footer-newslatter">
+							<input type="email" placeholder="E-mail">
+							<button class="site-btn">Subscribe</button>
+							<p>*We don’t spam</p>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer> 
+	<!-- footer section end -->
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/mixitup.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/main.js"></script>
+</html>
